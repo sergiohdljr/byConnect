a585ef9e7bb89e5822574f05e9d8d15a10c1d1d8
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-26 19:19:38 -0300

    feat: mock user and feed data

---
+++ src/data/index.js
@@ -0,0 +1,36 @@
+export const user = {
+  nome: "Sérgio H. de Lima Jr.",
+  username: "sergiohdljr",
+  fotoPerfil: "https://avatars.githubusercontent.com/u/102623806?v=4",
+};
+
+export const feed = [
+  {
+    fotoPerfilUsuario: "",
+    username: "",
+    texto: "",
+    foto: "",
+    datetime: "",
+  },
+  {
+    fotoPerfilUsuario: "",
+    username: "",
+    texto: "",
+    foto: "",
+    datetime: "",
+  },
+  {
+    fotoPerfilUsuario: "",
+    username: "",
+    texto: "",
+    foto: "",
+    datetime: "",
+  },
+  {
+    fotoPerfilUsuario: "",
+    username: "",
+    texto: "",
+    foto: "",
+    datetime: "",
+  },
+];

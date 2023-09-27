2c813e5e734a6a4e976853b0452620ddad3a8b84
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-27 19:10:09 -0300

    feat: listen event emit and create layout

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,51 @@
   <v-app>
     <v-main>
       <ToolbarComponent :data-user="user" />
+      <v-container fluid>
+        <v-row justify="center">
+          <v-col cols="12" sm="11" md="8" lg="6">
+            <FormPostComponent
+              @Post="Publicar"
+              :user-profile="user.fotoPerfil"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import ToolbarComponent from "./components/ToolbarComponent.vue";
-import { user } from "./data";
+import FormPostComponent from "./components/FormPostComponent.vue";
 
 export default {
   name: "App",
 
-  components: { ToolbarComponent },
+  components: { ToolbarComponent, FormPostComponent },
 
   data() {
+    const user = {
+      nome: "Sérgio H. de Lima Jr.",
+      username: "sergiohdljr",
+      fotoPerfil: "https://avatars.githubusercontent.com/u/102623806?v=4",
+    };
     return {
       user,
+      feed: [],
     };
+  },
+  methods: {
+    Publicar(dados) {
+      const NovaPublicacao = {
+        texto: dados,
+        foto: "",
+        datetime: new Date(),
+        user: this.user,
+      };
+      this.feed.push(NovaPublicacao);
+      console.log(NovaPublicacao);
+    },
   },
 };
 </script>

a583be88b3e01c8d17971797f320ac05a9ecf881
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-29 15:04:32 -0300

    fix: optional render on image

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,21 +1,17 @@
 <template>
   <v-card class="mx-auto" outlined width="100%">
     <v-card-title>
-      <span class="subtitle-1">Sep 28 2023 18:53:02</span>
+      <span class="subtitle-1">{{ postData.datetime }}</span>
     </v-card-title>
 
     <v-card-text class="subtitle-1">
-      "Turns out semicolon-less style is easier and safer in TS because most
-      gotcha edge cases are type invalid as well." Lorem ipsum dolor sit amet,
-      consectetur adipisicing elit. Ex deserunt est odit perferendis ipsa itaque
-      fuga dolores necessitatibus explicabo repellat consequatur asperiores
-      perspiciatis atque quam obcaecati quas, facilis rem in?
+      <p>{{ postData.texto }}</p>
     </v-card-text>
-    <v-list-item>
+    <v-list-item v-if="postData.foto.lenght === 0">
       <v-img
-        lazy-src="https://picsum.photos/id/11/10/6"
+        v-if="postData.foto.lenght === 0"
         width="100%"
-        src="https://picsum.photos/id/11/500/300"
+        :src="postData.foto"
       ></v-img>
     </v-list-item>
 
@@ -25,12 +21,14 @@
           <v-img
             class="elevation-6"
             alt=""
-            src="https://avataaars.io/?avatarStyle=Transparent&topType=ShortHairShortCurly&accessoriesType=Prescription02&hairColor=Black&facialHairType=Blank&clotheType=Hoodie&clotheColor=White&eyeType=Default&eyebrowType=DefaultNatural&mouthType=Default&skinColor=Light"
+            :src="postData.user.fotoPerfil"
           ></v-img>
         </v-list-item-avatar>
         <v-list-item-content>
-          <v-list-item-title class="h1">Evan You</v-list-item-title>
-          <v-list-item-title>@EvanYou</v-list-item-title>
+          <v-list-item-title class="h1">{{
+            postData.user.nome
+          }}</v-list-item-title>
+          <v-list-item-title>@{{ postData.user.username }}</v-list-item-title>
         </v-list-item-content>
         <v-row align="center" justify="end">
           <EditDialogComponent></EditDialogComponent>
@@ -47,6 +45,9 @@ import DeleteDialog from "./Dialogs/DeleteDialog";
 
 export default {
   components: { EditDialogComponent, DeleteDialog },
+  props: {
+    postData: Object,
+  },
   data() {
     return {};
   },

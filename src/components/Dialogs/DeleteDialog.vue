cec5e0982bac5eb467b3fd117657ea151290f1d6
sergiohdljr <sergio.tjf1@gmail.com>
2023-09-29 14:45:15 -0300

    feat: creating delete dialog and delete dialog

---
+++ src/components/Dialogs/DeleteDialog.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="red" class="rounded-xxl" dark v-bind="attrs" v-on="on">
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-text>
+        <v-container>
+          <h2>Tem certeza que deseja deletar o seu Post?</h2>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="green darken-1" text @click="dialog = false"> Não </v-btn>
+        <v-btn color="red darken-1" text @click="dialog = false">
+          Sim, deletar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Dialogs/EditDialogComponent.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-dialog v-model="dialog" persistent max-width="600px">
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn color="green" class="rounded-xxl" dark v-bind="attrs" v-on="on">
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-card-title>
+        <span class="text-h5">Edite o seu Post</span>
+      </v-card-title>
+      <v-card-text>
+        <v-container>
+          <v-textarea counter label="Edite o texto do seu post"></v-textarea>
+        </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="red darken-1" text @click="dialog = false">
+          Cancelar
+        </v-btn>
+        <v-btn color="green darken-1" text @click="dialog = false">
+          Salvar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    };
+  },
+};
+</script>
+
+<style></style>

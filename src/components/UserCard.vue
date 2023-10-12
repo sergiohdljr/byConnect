5563de3ff2143eae2cff460549baf5d1f23078a2
sergiohdljr <sergio.tjf1@gmail.com>
2023-10-12 11:44:28 -0300

    fix: add max-width to card

---
+++ src/components/UserCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-card class="mx-auto" max-width="600" width="100%" outlined>
+    <v-list-item three-line>
+      <v-list-item-content>
+        <v-list-item-title class="text-h5 mb-1">
+          Nome <v-icon>mdi-eye-off</v-icon> <v-icon>mdi-eye</v-icon>
+          <div class="text-overline mb-4">@username</div>
+        </v-list-item-title>
+      </v-list-item-content>
+
+      <v-list-item-avatar tile size="80" color="grey"></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn outlined rounded text>visualizar perfil de Usuário </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
--- src/views/AllUsersView.vue
+++ src/views/AllUsersView.vue
@@ -1,3 +1,14 @@
 <template>
-  <h2>todos os usuários</h2>
+  <div>
+    <h2>todos os usuários</h2>
+    <UserCard />
+  </div>
 </template>
+<script>
+import UserCard from "../components/UserCard.vue";
+export default {
+  components: {
+    UserCard,
+  },
+};
+</script>

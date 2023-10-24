107cb04a656f65c4e3b6a669deb4ab3036bf38b0
sergiohdljr <sergio.tjf1@gmail.com>
2023-10-24 15:02:32 -0300

    feat: add chunks for login and register

--- src/router/chunks.js
+++ src/router/chunks.js
@@ -3,4 +3,6 @@ export const routesViews = {
   Users: () => import("../views/UsersView.vue"),
   UserProfile: () => import("../views/UserProfileView.vue"),
   Posts: () => import("../views/PostsView.vue"),
+  Login: () => import("../views/LoginView.vue"),
+  Register: () => import("../views/RegisterView.vue"),
 };

0752b21ea033ece0975a5d6c5cde8aa6962e22fa
sergiohdljr <sergio.tjf1@gmail.com>
2023-10-24 19:16:09 -0300

    fix: protect routes - allow user go to register and login only

--- src/router/index.js
+++ src/router/index.js
@@ -11,31 +11,37 @@ const routes = [
     path: "/",
     name: "Timeline",
     component: routesViews.TimeLine,
+    meta: { requiresAuth: true },
   },
   {
     path: "/Users",
     name: "Users",
     component: routesViews.Users,
+    meta: { requiresAuth: true },
   },
   {
     path: "/User-profile/:username",
     name: "UserProfile",
     component: routesViews.UserProfile,
+    meta: { requiresAuth: true },
   },
   {
     path: "/Posts/:username",
     name: "Posts",
     component: routesViews.Posts,
+    meta: { requiresAuth: true },
   },
   {
     path: "/login",
     name: "Login",
     component: routesViews.Login,
+    meta: { requiresAuth: false },
   },
   {
     path: "/register",
     name: "Register",
     component: routesViews.Register,
+    meta: { requiresAuth: false },
   },
 ];
 
@@ -46,8 +52,13 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.name !== "Login" && !isAuthenticated) next({ name: "Login" });
-  else next();
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && !isAuthenticated) {
+    next({ name: "Login" });
+  } else {
+    next();
+  }
 });
 
 export default router;

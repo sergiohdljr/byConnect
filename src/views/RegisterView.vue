9f721740357a3401b69dcb0b284f35fb8270234a
sergiohdljr <sergio.tjf1@gmail.com>
2023-10-24 15:03:30 -0300

    feat: create register page

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-form class="form">
+    <h3 class="text-h4">Registre-se</h3>
+    <div>
+      <v-text-field type="email" label="E-mail" required />
+      <v-text-field type="password" label="Senha" required />
+    </div>
+    <v-btn type="submit" color="primary text-capitalize text-subtitle-1"
+      >Registre-se</v-btn
+    >
+    <router-link to="/login">Login</router-link>
+  </v-form>
+</template>

5dcabbea2e9a8d758073468afd4bd10ad64852c3
AyrtonF <ayrtonleonardo@gmail.com>
2023-10-25 14:35:32 -0300

    feat: create new buttons to login page

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -5,9 +5,18 @@
       <v-text-field type="email" label="E-mail" required />
       <v-text-field type="password" label="Senha" required />
     </div>
-    <v-btn type="submit" color="primary text-capitalize text-subtitle-1"
-      >Login</v-btn
-    >
+    <div class="group-btn">
+      <v-btn type="submit" color="primary text-none text-subtitle-1"
+        >Login</v-btn
+      >
+      <v-btn type="submit" color="primary text-none text-subtitle-1"
+        >Entrar com Google<v-icon class="ml-2">mdi-google</v-icon></v-btn
+      >
+      <v-btn type="submit" color="primary text-none text-subtitle-1"
+        >Entrar com Github<v-icon class="ml-2">mdi-github</v-icon></v-btn
+      >
+    </div>
+
     <router-link to="/register">Registre-se</router-link>
   </v-form>
 </template>
@@ -28,5 +37,16 @@
 
 .form > h3 {
   margin-bottom: 4rem;
+}
+
+.group-btn {
+  display: flex;
+  flex-direction: column;
+  gap: 0.2rem;
+}
+
+.btn-credential {
+  display: flex !important;
+  gap: 0.3rem !important;
 }
 </style>

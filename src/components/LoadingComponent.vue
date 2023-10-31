3c0f212a608cc195546dc9f1f7b838ba49ead552
sergiohdljr <sergio.tjf1@gmail.com>
2023-10-31 18:28:44 -0300

    feat: create loading component

---
+++ src/components/LoadingComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-h-100">
+    <div>
+      <p>carregando...</p>
+      <v-progress-circular
+        size="40"
+        color="primary"
+        indeterminate
+      ></v-progress-circular>
+    </div>
+  </div>
+</template>
+<style>
+.w-h-100 {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.w-h-100 > div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+</style>

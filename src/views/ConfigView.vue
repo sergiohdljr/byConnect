340102739b0f050710a9ba7b447d2b8edb2f3ba4
sergiohdljr <sergio.tjf1@gmail.com>
2023-11-05 21:38:24 -0300

    fix: css of config page

--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -7,15 +7,17 @@
     <v-btn @click="$router.push('/UpdateUser')"
       >Editar Informações do usuário</v-btn
     >
-    <v-row class="mt-15 d-flex flex-column">
+    <v-row class="pa-3 mt-10 d-flex flex-column">
       <div class="d-flex flex-row align-end">
         <h3 class="d-flex flex-row align-end">Deletar Usuário</h3>
       </div>
       <p>
         Ao deletar sua conta você terá todas suas informações excluídas da
-        plataforma.
+        plataforma. Para excluir uma conta ela deve ter um login recente.
       </p>
-      <v-btn color="red" @click="goToDeleteUser">Deletar conta</v-btn>
+      <div>
+        <v-btn color="red" @click="goToDeleteUser">Deletar conta</v-btn>
+      </div>
     </v-row>
   </v-container>
 </template>

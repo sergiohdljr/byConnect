4932dfc8534bc41e13aef2f7ae83c50a21986a0f
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-02 15:57:39 -0300

    feat: styles comments

---
+++ src/components/CommentsList.vue
@@ -0,0 +1,32 @@
+<template>
+    <v-list three-line>
+      <template v-for="item in listaComentarios" :key="item.id">
+        <v-list-item >
+          <v-list-item-avatar>
+            <v-img :src="item.user.fotoPerfil"></v-img>
+          </v-list-item-avatar>
+  
+          <v-list-item-content>
+            <v-list-item-title>
+              <router-link :to="`/User-profile/${item.user.email}`">
+                {{ item.user.nome }}
+              </router-link>
+            </v-list-item-title>
+            <v-list-item-title>{{ item.user.email }} </v-list-item-title>
+            <v-list-item-subtitle
+              >{{ item.textoComentario }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+      </template>
+    </v-list>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      listaComentarios: Array,
+    },
+  };
+  </script>
+  

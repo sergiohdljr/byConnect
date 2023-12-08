0010766b4fba915404496f22b635b3ab346723ad
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-08 20:31:45 -0300

    feat: change primary color to icon colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,13 @@ Vue.use(Vuetify);
 export default new Vuetify({
   theme: {
     dark: true,
+    themes: {
+      dark: {
+        primary: "#F19F05",
+      },
+      light: {
+        primary: "#F19F05",
+      },
+    },
   },
 });

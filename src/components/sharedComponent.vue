2f83e2d487c38a1ca28891b98c71fd270e5ef288
sergiohdljr <sergio.tjf1@gmail.com>
2023-12-09 11:49:54 -0300

    feat: shared by component

---
+++ src/components/sharedComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-icon color="#808080">mdi-share</v-icon>
+    <span style="font-size: 16px; opacity: 0.8; color: gray">
+      <router-link :to="`/User-profile/${sharedBy}`">
+        {{ sharedBy }} compartilhou
+      </router-link>
+    </span>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    sharedBy: String,
+  },
+};
+</script>
